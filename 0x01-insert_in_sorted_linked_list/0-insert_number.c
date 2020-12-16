88fe49385c89d60d678032057fbd17bd32abd1ed
Viet Tran <vietnamtran@gmail.com>
2020-12-16 17:55:50 +0000

    Check double dereference then dereference head

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -13,7 +13,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new;
 	listint_t *current;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
@@ -23,7 +23,7 @@ listint_t *insert_node(listint_t **head, int number)
 	new->n = number;
 	new->next = NULL;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
