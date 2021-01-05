3e0820f998b3c03d70d5d8a75adef1025f56306e
Viet Tran <vietnamtran@gmail.com>
2021-01-05 18:05:38 +0000

    Add code for function

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-def minOperations(n)
+"""Function to find or help find minimum operations"""
+
+
+def minOperations(n):
+    """
+        Calculates the fewest number of operations needed to result
+        in exactly n H characters in the file.
+    """
+    if n < 2:
+        return 0
+
+    summation = []
+
+    while n % 2 == 0:
+        summation.append(2)
+        n = n // 2
+
+    for i in range(3, n + 1, 2):
+        while n % i == 0:
+            summation.append(i)
+            n = n // i
+
+    return (sum(summation))
