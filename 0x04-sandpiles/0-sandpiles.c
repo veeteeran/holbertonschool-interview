6105c2c5fa047f72f2218f13afac3df94eb0ec67
Viet Tran <vietnamtran@gmail.com>
2021-01-12 19:08:55 +0000

    Delete comments, appease betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,10 +1,11 @@
 #include "sandpiles.h"
-
-void corner_check(int grid[3][3]);
-void edge_check(int grid[3][3]);
-void middle_check(int grid[3][3]);
-static void print_grid(int grid[3][3]);
-
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: 3x3 grid added to and printed
+ * @grid2: 3x3 grid adding to grid1
+ *
+ * Return: void
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j, row = 3, col = 3;
@@ -15,10 +16,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		for (j = 0; j < col; j++)
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
 	}
-	
-/*	printf("=\n");
-	print_grid(grid1);
-*/
+
 	while (true)
 	{
 		corner = grid1[0][0];
@@ -26,22 +24,26 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		middle = grid1[1][1];
 		if (corner <= 3 && edge <= 3 && middle <= 3)
 			break;
-		else
-		{
-			printf("=\n");
-                	print_grid(grid1);
-		}
 
-		corner_check(grid1);
-		edge_check(grid1);
-		middle_check(grid1);
-
-/*		printf("=\n");
+		printf("=\n");
 		print_grid(grid1);
-*/
+		/* Only run functions when original assignments are true */
+		if (corner > 3)
+			corner_check(grid1);
+		if (edge > 3)
+			edge_check(grid1);
+		if (middle > 3)
+			middle_check(grid1);
+
 	}
 }
 
+/**
+ * corner_check - topples each corner, adds to edge
+ * @grid: 3x3 grid
+ *
+ * Return: void
+ */
 void corner_check(int grid[3][3])
 {
 	int corner = grid[0][0];
@@ -61,81 +63,73 @@ void corner_check(int grid[3][3])
 	}
 }
 
-void  edge_check(int grid[3][3])
+/**
+ * edge_check - topples each edge, adds to corners and middle
+ * @grid: 3x3 grid
+ *
+ * Return: void
+ */
+void edge_check(int grid[3][3])
 {
-        int edge = grid[0][1];
+	int edge = grid[0][1];
 
-        if (edge > 3)
-        {
-                /* Edges */
-                grid[0][1] -= 4;
-                grid[1][0] -= 4;
-                grid[1][2] -= 4;
-                grid[2][1] -= 4;
+	if (edge > 3)
+	{
+		/* Edges */
+		grid[0][1] -= 4;
+		grid[1][0] -= 4;
+		grid[1][2] -= 4;
+		grid[2][1] -= 4;
 		/* Corners */
-                grid[0][0] += 2;
-                grid[0][2] += 2;
-                grid[2][0] += 2;
-                grid[2][2] += 2;
+		grid[0][0] += 2;
+		grid[0][2] += 2;
+		grid[2][0] += 2;
+		grid[2][2] += 2;
 		/* Middle */
 		grid[1][1] += 4;
-        }
+	}
 }
 
+/**
+ * middle_check - topples the middle, adds to edges
+ * @grid: 3x3 grid
+ *
+ * Return: void
+ */
 void middle_check(int grid[3][3])
 {
-        int middle = grid[1][1];
+	int middle = grid[1][1];
 
-        if (middle > 3)
-        {
+	if (middle > 3)
+	{
 		/* Middle */
-                grid[1][1] -= 4;
-                /* Edges */
-                grid[0][1] += 1;
-                grid[1][0] += 1;
-                grid[1][2] += 1;
-                grid[2][1] += 1;
-        }
+		grid[1][1] -= 4;
+		/* Edges */
+		grid[0][1] += 1;
+		grid[1][0] += 1;
+		grid[1][2] += 1;
+		grid[2][1] += 1;
+	}
 }
-/*
-static void print_grid_sum(int grid1[3][3], int grid2[3][3])
-{
-    int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid1[i][j]);
-        }
-
-        printf(" %c ", (i == 1 ? '+' : ' '));
-
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid2[i][j]);
-        }
-        printf("\n");
-    }
-}
-*/
-
+/**
+ * print_grid - prints the grid
+ * @grid: 3x3 grid
+ *
+ * Return: void
+ */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+			printf(" ");
+			printf("%d", grid[i][j]);
+		}
+			printf("\n");
+	}
 }
