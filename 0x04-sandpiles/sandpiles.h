3729d59ca0ac99720ce75c5e4ed0f1f30e2a34b4
Viet Tran <vietnamtran@gmail.com>
2021-01-12 19:14:16 +0000

    Update header, add mains dir

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -3,7 +3,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
+static void print_grid(int grid[3][3]);
+void corner_check(int grid[3][3]);
+void edge_check(int grid[3][3]);
+void middle_check(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
 #endif /* SANDPILES_H */
