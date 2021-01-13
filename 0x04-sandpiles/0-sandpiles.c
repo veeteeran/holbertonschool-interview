1790e08d37c00744d20b3f1fa79f5f43bf2d57ba
Viet Tran <vietnamtran@gmail.com>
2021-01-13 15:23:13 +0000

    Add else to condition inside loop

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -20,6 +20,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
 			if (grid1[i][j] <= 3)
 				stable = true;
+			else
+				stable = false;
 		}
 	}
 
