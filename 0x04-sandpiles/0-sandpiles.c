23fac6d643b721a9587f5a06250ee7e6ab6afbf8
Viet Tran <vietnamtran@gmail.com>
2021-01-13 15:07:08 +0000

    Fix logic, of setting stable in first loop

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -18,7 +18,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		for (j = 0; j < 3; j++)
 		{
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
-			if (grid1[i][j] <= 3)	
+			if (grid1[i][j] < 3)
 				stable = true;
 		}	
 	}
