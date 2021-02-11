b669159155fd81d58955bc2180900d1a60acbbbc
Viet Tran <vietnamtran@gmail.com>
2021-02-11 16:50:37 +0000

    Add more efficient version

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -6,34 +6,16 @@
  */
 int is_palindrome(unsigned long n)
 {
-	unsigned long divisor = 1000000000000000000;
-	unsigned long first_digit = n / divisor;
-	unsigned long mod = 10, last_digit;
+	unsigned long bizzaro = 0, superboy;
 
-	if (n < 10)
-		return (1);
-
-	/* Find tens place */
-	while (first_digit == 0)
+	/* Clone of n so we don't mutate the original */
+	superboy = n;
+	while (superboy)
 	{
-		divisor /= 10;
-		first_digit = n / divisor;
+		/* Create a backwards version of n */
+		bizzaro = (bizzaro * 10) + (superboy % 10);
+		superboy /= 10;
 	}
-
-	last_digit =  n % mod;
-	while (first_digit == last_digit)
-	{
-		first_digit = n / divisor;
-		last_digit =  n % mod;
-
-		/* Get rid of first digit */
-		n -= divisor * first_digit;
-		/* Get rid of first digit */
-		n /= 10;
-		/* Reduce divisor by two tens places */
-		divisor /= 100;
-		if (divisor < mod)
-			return (1);
-	}
-	return (0);
+	/* Compare the original with the backwards copy */
+	return (bizzaro == n);
 }
