1c3d0cd9062fb9f44b7e79adf5e21103a8281f35
Viet Tran <vietnamtran@gmail.com>
2021-02-17 22:08:59 -0600

    Fix tab on last condition

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,6 +3,7 @@
 Determines if a given data set represents a valid UTF-8 encoding
 """
 
+
 def validUTF8(data):
     if data == []:
         return True
@@ -17,38 +18,39 @@ def validUTF8(data):
         if d < 128:
             index += 1
             continue
-        try:
-            # bitmask to pull least significant 8 bits
-            d = data[index] & mask
-            # This range signifies a 2 byte block
-            if d > 191 and d < 224:
-                byte_two = data[index + 1] & mask
-                if byte_two > 127 and byte_two < 192:
-                    index += 1
-                    continue
-                else:
-                    return False
-            # This range signifies a 3 byte block
-            if d > 223 and d < 240:
-                byte_two = data[index + 1] & mask
-                byte_three = data[index + 2] & mask
-                if (byte_two > 127 and byte_two < 192) and (byte_three > 127 and byte_three < 192):
-                    index += 2
-                    continue
-                else:
-                    return False
-            # This range signifies a 4 byte block
-            if d > 239 and d < 248:
-                byte_two = data[index + 1] & mask
-                byte_three = data[index + 2] & mask
-                byte_four = data[index + 3] & mask
-                if (byte_two > 127 and byte_two < 192) and (byte_three > 127 and byte_three < 192) and (byte_four > 127 and byte_four < 192):
-                    index += 3
-                    continue
-            else:
-                    return False
-        except:
-            return False
+        else:
+            try:
+                # bitmask to pull least significant 8 bits
+                d = data[index] & mask
+                # This range signifies a 2 byte block
+                if d > 191 and d < 224:
+                    byte_two = data[index + 1] & mask
+                    if byte_two > 127 and byte_two < 192:
+                        index += 1
+                        continue
+                    else:
+                        return False
+                # This range signifies a 3 byte block
+                if d > 223 and d < 240:
+                    byte_two = data[index + 1] & mask
+                    byte_three = data[index + 2] & mask
+                    if (byte_two > 127 and byte_two < 192) and (byte_three > 127 and byte_three < 192):
+                        index += 2
+                        continue
+                    else:
+                        return False
+                # This range signifies a 4 byte block
+                if d > 239 and d < 248:
+                    byte_two = data[index + 1] & mask
+                    byte_three = data[index + 2] & mask
+                    byte_four = data[index + 3] & mask
+                    if (byte_two > 127 and byte_two < 192) and (byte_three > 127 and byte_three < 192) and (byte_four > 127 and byte_four < 192):
+                        index += 3
+                        continue
+                    else:
+                        return False
+            except:
+                return False
         index += 1
 
     return True
