ca75dceb0bcbec6dec61de9fcfb1688700d0b1a5
Viet Tran <vietnamtran@gmail.com>
2021-02-17 21:52:12 -0600

    Change i to index

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -19,10 +19,10 @@ def validUTF8(data):
             continue
         try:
             # bitmask to pull least significant 8 bits
-            d = data[i] & mask
+            d = data[index] & mask
             # This range signifies a 2 byte block
             if d > 191 and d < 224:
-                byte_two = data[i + 1] & mask
+                byte_two = data[index + 1] & mask
                 if byte_two > 127 and byte_two < 192:
                     index += 1
                     continue
@@ -30,8 +30,8 @@ def validUTF8(data):
                     return False
             # This range signifies a 3 byte block
             if d > 223 and d < 240:
-                byte_two = data[i + 1] & mask
-                byte_three = data[i + 2] & mask
+                byte_two = data[index + 1] & mask
+                byte_three = data[index + 2] & mask
                 if (byte_two > 127 and byte_two < 192) and (byte_three > 127 and byte_three < 192):
                     index += 2
                     continue
@@ -39,9 +39,9 @@ def validUTF8(data):
                     return False
             # This range signifies a 4 byte block
             if d > 239 and d < 248:
-                byte_two = data[i + 1] & mask
-                byte_three = data[i + 2] & mask
-                byte_four = data[i + 3] & mask
+                byte_two = data[index + 1] & mask
+                byte_three = data[index + 2] & mask
+                byte_four = data[index + 3] & mask
                 if (byte_two > 127 and byte_two < 192) and (byte_three > 127 and byte_three < 192) and (byte_four > 127 and byte_four < 192):
                     index += 3
                     continue
