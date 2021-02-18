123e47348c8f971cfa334cd4e4db8a954b463f5b
Viet Tran <vietnamtran@gmail.com>
2021-02-18 12:02:15 -0600

    Refactor using try except else

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -13,48 +13,47 @@ def validUTF8(data):
     index = 0
     while (index < length):
         # bitmask to pull least significant 8 bits
-        d = data[index] & mask
+#        d = data[index] & mask
         # Valid 1 byte values
-        if d < 128:
-            index += 1
-            continue
+#        if d < 128:
+#            index += 1
+#            continue
+#        else:
+        try:
+            # bitmask to pull least significant 8 bits
+            d = data[index] & mask
+            # This range signifies a 2 byte block
+            if d > 191 and d < 224:
+                byte_two = data[index + 1] & mask
+                if byte_two > 127 and byte_two < 192:
+                    index += 2
+#                    continue
+                else:
+                    return False
+            # This range signifies a 3 byte block
+            if d > 223 and d < 240:
+                byte_two = data[index + 1] & mask
+                byte_three = data[index + 2] & mask
+                if (byte_two > 127 and byte_two < 192)\
+                        and (byte_three > 127 and byte_three < 192):
+                    index += 3
+#                    continue
+                else:
+                    return False
+            # This range signifies a 4 byte block
+            if d > 239 and d < 248:
+                byte_two = data[index + 1] & mask
+                byte_three = data[index + 2] & mask
+                byte_four = data[index + 3] & mask
+                if (byte_two > 127 and byte_two < 192)\
+                        and (byte_three > 127 and byte_three < 192)\
+                        and (byte_four > 127 and byte_four < 192):
+                    index += 4
+#                    continue
+                else:
+                    return False
+        except:
+            return False
         else:
-            try:
-                # bitmask to pull least significant 8 bits
-                d = data[index] & mask
-                # This range signifies a 2 byte block
-                if d > 191 and d < 224:
-                    byte_two = data[index + 1] & mask
-                    if byte_two > 127 and byte_two < 192:
-                        index += 2
-                        continue
-                    else:
-                        return False
-                # This range signifies a 3 byte block
-                if d > 223 and d < 240:
-                    byte_two = data[index + 1] & mask
-                    byte_three = data[index + 2] & mask
-                    if (byte_two > 127 and byte_two < 192)\
-                            and (byte_three > 127 and byte_three < 192):
-                        index += 3
-                        continue
-                    else:
-                        return False
-                # This range signifies a 4 byte block
-                if d > 239 and d < 248:
-                    byte_two = data[index + 1] & mask
-                    byte_three = data[index + 2] & mask
-                    byte_four = data[index + 3] & mask
-                    if (byte_two > 127 and byte_two < 192)\
-                            and (byte_three > 127 and byte_three < 192)\
-                            and (byte_four > 127 and byte_four < 192):
-                        index += 4
-                        continue
-                    else:
-                        return False
-            
-                index += 1
-            except:
-                return False
-
+            index += 1
     return True
