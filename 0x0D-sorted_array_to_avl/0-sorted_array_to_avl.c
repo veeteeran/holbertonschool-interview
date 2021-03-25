9ebc1f67539f8ce806cc7149df7afcf2fe784dfe
Viet <vietnamtran@gmail.com>
2021-03-24 22:57:32 -0500

    Add tested code

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,33 +10,53 @@
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {     
         avl_t *tree = NULL;
+	int flag = 1;
 
         if (!array)
                 return (NULL); 
 
-        create_tree(tree, array, size - 1);
+        create_tree(&tree, array, size, flag);
     
         return (tree);
 }
-    
-avl_t *create_tree(avl_t *tree, int *array, size_t size)
-{
-        size_t mid = size / 2;
+/**
+ * create_tree - creates an AVL tree
+ * @tree: pointer to the tree 
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of elements in the array
+ * @flag: determines whether recursion goes left or right
+ *
+ * Return: pointer to the root node of the created AVL tree, or NULL on failure
+ **/
+avl_t *create_tree(avl_t **tree, int *array, size_t size, int flag)
+{            
+        size_t mid = (size - 1) / 2;
+        avl_t *node = node_maker(array[mid]);
 
-        printf("size: %d\n", (int)size);
         if (size == 0)
-        {
-                tree->left = node_maker(array[mid]);
-                printf("tree->left: %d\n", tree->left->n);
-                return (tree);
-        }
+                return (*tree);
 
-	tree->left = create_tree(tree, array, mid - 1);
-	tree->right = create_tree(tree, array, mid + 1);
+        node->parent = *tree;
 
-	return (tree);
+        if (!*tree)
+                *tree = node;
+        else if (flag == 1)
+                (*tree)->left = node;
+        else
+                (*tree)->right = node;
+
+        create_tree(&node, array, mid, 1);
+        create_tree(&node, &array[mid + 1], size - (mid + 1), 0);
+
+        return (*tree);
 }
 
+/*
+ * node_maker - creates a node to an AVL tree
+ * @n: data contained in node
+ *
+ * Return: node or NULL on failure
+ **/
 avl_t *node_maker(int n)
 {
         avl_t *node;
