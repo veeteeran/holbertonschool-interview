477974fccc9de174beb3b1960bf3c62dc02f07d1
Viet <vietnamtran@gmail.com>
2021-03-25 15:24:19 -0500

    Betty fixes

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,28 +1,28 @@
 #include "binary_trees.h"
 
-/**       
+/**
  * sorted_array_to_avl - Builds an AVL tree from an array
  * @array: pointer to the first element of the array to be converted
  * @size: number of elements in the array
- *       
+ *
  * Return: pointer to the root node of the created AVL tree, or NULL on failure
  **/
 avl_t *sorted_array_to_avl(int *array, size_t size)
-{     
-        avl_t *tree = NULL;
+{
+	avl_t *tree = NULL;
 	int flag = 1;
 
-        if (!array)
-                return (NULL); 
+	if (!array)
+                return (NULL);
 
-        create_tree(&tree, array, size, flag);
-    
-        return (tree);
+	create_tree(&tree, array, size, flag);
+
+	return (tree);
 }
 
 /**
  * create_tree - creates an AVL tree
- * @tree: pointer to the tree 
+ * @tree: pointer to the tree
  * @array: pointer to the first element of the array to be converted
  * @size: number of elements in the array
  * @flag: determines whether recursion goes left or right
@@ -30,9 +30,9 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
  * Return: pointer to the root node of the created AVL tree, or NULL on failure
  **/
 avl_t *create_tree(avl_t **tree, int *array, size_t size, int flag)
-{            
-        size_t mid = (size - 1) / 2;
-        avl_t *node = malloc(sizeof(avl_t));
+{
+	size_t mid = (size - 1) / 2;
+	avl_t *node = malloc(sizeof(avl_t));
 
 	if (!node)
 	{
@@ -41,26 +41,26 @@ avl_t *create_tree(avl_t **tree, int *array, size_t size, int flag)
 	}
 
 	node->n = array[mid];
-        node->parent = NULL;
-        node->left = NULL;
-        node->right = NULL;
+	node->parent = NULL;
+	node->left = NULL;
+	node->right = NULL;
 
-        if (size == 0)
-                return (*tree);
+	if (size == 0)
+		return (*tree);
 
-        node->parent = *tree;
+	node->parent = *tree;
 
-        if (!*tree)
-                *tree = node;
-        else if (flag == 1)
-                (*tree)->left = node;
-        else
-                (*tree)->right = node;
+	if (!*tree)
+		*tree = node;
+	else if (flag == 1)
+		(*tree)->left = node;
+	else
+		(*tree)->right = node;
 
-        create_tree(&node, array, mid, 1);
-        create_tree(&node, &array[mid + 1], size - (mid + 1), 0);
+	create_tree(&node, array, mid, 1);
+	create_tree(&node, &array[mid + 1], size - (mid + 1), 0);
 
-        return (*tree);
+	return (*tree);
 }
 
 /**
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -16,10 +16,10 @@
  */
 typedef struct binary_tree_s
 {
-        int n;
-        struct binary_tree_s *parent;
-        struct binary_tree_s *left;
-        struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
 
