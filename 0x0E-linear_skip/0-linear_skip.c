a4676caeb7d5a30d25bc805424c563ecc58d6b3f
Viet <vietnamtran@gmail.com>
2021-03-29 07:27:58 -0500

    Initial commit

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,12 @@
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ * @list: pointer to the head of the skip list
+ * @value: value to search for
+ *
+ * Return: pointer to the first node where value is located or NULL
+ **/
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	return (NULL);
+}
---
+++ 0x0E-linear_skip/README.md
@@ -0,0 +1,6 @@
+# 0x0E. Linear search in skip list
+
+---
+
+## Author
+* **Viet Tran** - [veeteeran](https://github.com/veeteeran)
