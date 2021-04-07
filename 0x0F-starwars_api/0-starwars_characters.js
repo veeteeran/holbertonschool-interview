66ae7f3c5a628996ba2903b590fb4848b535b558
veeteeran <vietnamtran@gmal.com>
2021-04-07 19:58:44 +0000

    Add code to console log in order

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID + '/';
+
+request.get(url, async (error, response, body) => {
+  if (error) { return; }
+
+  const data = JSON.parse(body);
+
+  const getName = charURLS => {
+    const promise = new Promise((resolve, reject) => {
+      request.get(charURLS, (error, response, body) => {
+        if (error) { reject(error); } else { resolve(body); }
+      });
+    });
+    return promise;
+  };
+
+  for (let i = 0; i < (data.characters).length; i++) {
+    const result = await getName(data.characters[i]);
+    console.log(JSON.parse(result).name);
+  }
+});
