d01bf09af96799f0f861d43dfc2c2d04fd66c183
veeteeran <vietnamtran@gmail.com>
2021-04-14 14:12:29 +0000

    Fix pycodestyle error

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -5,15 +5,10 @@
 def rain(walls):
     """Rain interview question"""
     rain = 0
-    # Loop through the array
     for i in range(1, len(walls)):
-    # At each index from start to index -> left wall getMax
         left = max(walls[0:i + 1])
-    # From index to end -> right wall getMax
         right = max(walls[i:len(walls)])
-    # Get min(left_wall, right_wall)
         minHeight = min(left, right)
-    # rain_water += min(left_wall, right_wall) - arr[i]
         if minHeight - walls[i] < 0:
             continue
         else:
