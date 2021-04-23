4bb2f2f4182df49b21089ab4dbcdcd4850cde21d
veeteeran <vietnamtran@gmail.com>
2021-04-23 20:49:06 +0000

    Update header

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t size, size_t index);
-void swap(int *array, int *parent, int *child);
+void heapify(int *array, size_t size, size_t index, size_t sub_size);
+void swap(int *array, size_t size, int *parent, int *child);
 void print_array(const int *array, size_t size);
 #endif /* _SORT_H_ */
