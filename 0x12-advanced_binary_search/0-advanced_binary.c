da3488a7ff5f612b03a45484c407f57e5d288ce0
veeteeran <vietnamtran@gmail.com>
2021-04-29 22:27:51 +0000

    Fix betty issues

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,24 +1,33 @@
 #include "search_algos.h"
-int helper(int *array, int start, int end, int value);
+
 /**
- * advanced_binary_search - searches for a value in a sorted array of
- * integers using the Binary search algorithm
+ * advanced_binary - calls helper function
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: the value to search for
  *
- * Return: the first index where value is located
+ * Return: the first index where value is located or -1 on failure
  */
 int advanced_binary(int *array, size_t size, int value)
 {
 	int start = 0, end = size - 1;
 
 	if (array == NULL)
-                return (-1);
+		return (-1);
 
-	return helper(array, start, end, value);
+	return (helper(array, start, end, value));
 }
 
+/**
+ * helper - searches for a value in a sorted array of
+ * integers using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @start: start index
+ * @end: end index
+ * @value: the value to search for
+ *
+ * Return: the first index where value is located or -1 on failure
+ */
 int helper(int *array, int start, int end, int value)
 {
 	int mid, i;
@@ -39,9 +48,9 @@ int helper(int *array, int start, int end, int value)
 			return (mid);
 
 		if (array[mid] >= value)
-			return helper(array, start, mid, value);
+			return (helper(array, start, mid, value));
 
-		return helper(array, mid + 1, end, value);
+		return (helper(array, mid + 1, end, value));
 	}
 
 	return (-1);
--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -6,5 +6,6 @@
 #include <math.h>
 
 int advanced_binary(int *array, size_t size, int value);
+int helper(int *array, int start, int end, int value);
 
 #endif /* SEARCH_ALGOS_H */
