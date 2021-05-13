dcaace5d128cf9866cd7615efe0b82b49d8a1833
veeteeran <vietnamtran@gmail.com>
2021-05-13 19:22:25 +0000

    Add return 0

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * heap_extract -
+ * heap_extract - extracts the root node of a Max Binary Heap
  *
- * Return: 
+ * Return: value stored in the root node, 0 on failure
  **/
 
 int heap_extract(heap_t **root)
-{
-
+{
+	return (0);
 }
