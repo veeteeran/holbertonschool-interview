406c4c817d28ccbe2469d0fc01261e017db94e1e
veeteeran <vietnamtran@gmail.com>
2021-05-30 20:28:58 +0000

    Pep8 fixes

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,12 +3,12 @@
 
 
 def rotate_2d_matrix(matrix):
-	"""In place rotation of 2D matrix"""
-	nestedLen = len(matrix[0])
-	for i in range(nestedLen // 2):
-		for j in range(i, nestedLen - i - 1):
-			temp = matrix[i][j]
-			matrix[i][j] = matrix[nestedLen - 1 - j][i]
-			matrix[nestedLen - 1 - j][i] = matrix[nestedLen - 1 - i][nestedLen - 1 - j]
-			matrix[nestedLen - 1 - i][nestedLen - 1 - j] = matrix[j][nestedLen - 1 - i]
-			matrix[j][nestedLen - 1 - i] = temp
+    """In place rotation of 2D matrix"""
+    nested = len(matrix[0])
+    for i in range(nested // 2):
+        for j in range(i, nested - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[nested - 1 - j][i]
+            matrix[nested - 1 - j][i] = matrix[nested - 1 - i][nested - 1 - j]
+            matrix[nested - 1 - i][nested - 1 - j] = matrix[j][nested - 1 - i]
+            matrix[j][nested - 1 - i] = temp
