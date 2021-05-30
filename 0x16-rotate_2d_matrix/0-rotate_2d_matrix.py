98dd797c8c5942bb61a3abc879bd877bb84ff80a
veeteeran <vietnamtran@gmail.com>
2021-05-30 20:22:39 +0000

    Add rotate_2d_matrix

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""Given an n x n 2D matrix, rotate it 90 degrees clockwise"""
+
+
 def rotate_2d_matrix(matrix):
+	"""In place rotation of 2D matrix"""
+	nestedLen = len(matrix[0])
+	for i in range(nestedLen // 2):
+		for j in range(i, nestedLen - i - 1):
+			temp = matrix[i][j]
+			matrix[i][j] = matrix[nestedLen - 1 - j][i]
+			matrix[nestedLen - 1 - j][i] = matrix[nestedLen - 1 - i][nestedLen - 1 - j]
+			matrix[nestedLen - 1 - i][nestedLen - 1 - j] = matrix[j][nestedLen - 1 - i]
+			matrix[j][nestedLen - 1 - i] = temp
