fe3a88317209a17b02c9c771d988c9ff49b79eb1
veeteeran <vietnamtran@gmail.com>
2021-05-31 21:45:56 +0000

    Add find the loop

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,12 +1,34 @@
 #include "lists.h"
 
 /**
- * find_listint_loop -
+ * find_listint_loop - finds the loop in a linked list
+ * @head: pointer to linked list
  *
- * Return: 
+ * Return: address of the node where the loop starts, or NULL
  **/
-
 listint_t *find_listint_loop(listint_t *head)
-{
+{
+	listint_t *tortoise, *hare;
 
+	if (!head)
+		return (NULL);
+
+	tortoise = hare = head;
+	while (hare->next && hare->next->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (hare);
+		}
+	}
+	return (NULL);
 }
