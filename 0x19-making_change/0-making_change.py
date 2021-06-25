e1b19dd434c55a81fae6aa11d462ebbd37c8c7f7
veeteeran <vietnamtran@gmail.com>
2021-06-25 02:50:57 +0000

    Pep8 fixes

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -14,8 +14,8 @@ def makeChange(coins, total):
     for i in range(1, total + 1):
         s.append(-1)
         for coin in coins:
-            if i - coin >= 0 and s[i - coin] != -1
-and (s[i] > s[i - coin] or s[i] == -1):
+            if (i - coin >= 0 and s[i - coin] != -1 and
+               (s[i] > s[i - coin] or s[i] == -1)):
                 s[i] = 1 + s[i - coin]
 
-return s[total]
+    return s[total]
