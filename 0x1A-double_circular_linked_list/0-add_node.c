7a4a98aae1c69fce2b277b9b92c58bbbc8f3e738
Viet <vietnamtran@gmail.com>
2021-06-29 22:26:01 -0500

    Setup file

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,12 +1,26 @@
 #include "list.h"
 
 /**
- * add_node_end -
+ * add_node_end - add to end of circular linked list
+ * @list: the list to modify
+ * @str: the string to copy into the new node
  *
- * Return: 
+ * Return: Address of the new node, or NULL on failure
  **/
 
 List *add_node_end(List **list, char *str)
-{
+{
+
+}
+
+/**
+ * add_node_begin - add to beginning of circular linked list
+ * @list: the list to modify
+ * @str: the string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ **/
+List *add_node_begin(List **list, char *str)
+{
 
 }
