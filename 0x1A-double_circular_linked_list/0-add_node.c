f544433f14876a352aa8287468a4a7dadb8051f2
Viet <vietnamtran@gmail.com>
2021-06-29 22:18:18 -0500

    Initial commit

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,12 @@
+#include "list.h"
+
+/**
+ * add_node_end -
+ *
+ * Return: 
+ **/
+
+List *add_node_end(List **list, char *str)
+{
+
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,6 @@
+# 0x1A. Double Circular Linked List
+
+---
+
+## Author
+* **Viet Tran** - [veeteeran](https://github.com/veeteeran)
