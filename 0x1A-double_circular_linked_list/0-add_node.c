c045ef2e5ca0f4e2fe8f44d98bc7a22fa429db2f
Viet <vietnamtran@gmail.com>
2021-07-01 18:34:30 -0500

    Add code for interview question

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,5 +1,6 @@
 #include "list.h"
 
+int _strlen(char *s);
 /**
  * add_node_end - add to end of circular linked list
  * @list: the list to modify
@@ -10,7 +11,33 @@
 
 List *add_node_end(List **list, char *str)
 {
+	List *new, *last;
 
+	if (!list)
+		return (NULL);
+
+	new = malloc(sizeof(List));
+	if (!new)
+		return (NULL);
+
+	new->prev = NULL;
+	new->next = NULL;
+	new->str = strdup(str);
+	if (!*list)
+	{
+		*list = new;
+		new->prev = new->next;
+		new->next = new->prev;
+	}	
+	else
+	{
+		last = *list;
+		new->next = *list;
+		(*list)->prev = new;
+		new->prev = last;
+		last->next = new;
+	}
+	return (new);
 }
 
 /**
@@ -22,5 +49,48 @@ List *add_node_end(List **list, char *str)
  **/
 List *add_node_begin(List **list, char *str)
 {
+	List *new, *last;
 
+        if (!list)
+                return (NULL);
+
+        new = malloc(sizeof(List));
+        if (!new)
+                return (NULL);
+        
+        new->prev = NULL;
+        new->next = NULL;
+        new->str = strdup(str);
+        if (!*list)
+        {
+                *list = new;
+                new->prev = new->next;
+                new->next = new->prev;
+        }      
+	else
+        {       
+                last = *list;
+                new->next = *list;
+                new->prev = last;
+                last->next = (*list)->prev = new_node;
+		*list = new;
+        }
+        return (new);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ *
+ * Return: the length of a string as an int
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		continue;
+	}
+	return (counter);
 }
