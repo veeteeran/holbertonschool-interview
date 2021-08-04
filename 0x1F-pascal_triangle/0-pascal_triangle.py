936afb0257d190dba83796fb8a78d806ae080a7b
veeteeran <vietnamtran@gmail.com>
2021-08-04 00:09:16 +0000

    Add pascal triangle

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -4,7 +4,21 @@
 
 def pascal_triangle(n):
     """
-        Returns a list of lists of integers representing the
-        Pascal’s triangle of n
+        Returns a list of lists of integers representing
+        Pascal's triangle of n
     """
-    pass
+    result = []
+
+    if n <= 0:
+        return result
+
+    for row_num in range(n):
+        row = [None for _ in range(row_num + 1)]
+        row[0], row[-1] = 1, 1
+
+        for j in range(1, len(row) - 1):
+            row[j] = result[row_num - 1][j - 1] + result[row_num - 1][j]
+
+        result.append(row)
+
+    return result
